87b7454e7699ae4826e2986274bcb62cc979b6b3
Dougie Dobrzynski <doug@promethost.com>
2014-09-30 14:28:12 -0500

    Removing incomplete functionality on the Migration behat context.

--- src/Behat/SubContext/MigrationContext.php
+++ src/Behat/SubContext/MigrationContext.php
@@ -25,7 +25,10 @@ class MigrationContext extends SubContext
     $this->assertModuleExists($module, TRUE);
   }
 
-  // @AfterFeature @migration
+  /**
+   * TO-DO: Make this work.
+   * @AfterFeature @migration
+   */
   public static function rollbackMigration(FeatureEvent $event) {
     $migration_array = migrate_migrations();
     $migrations = array_keys($migration_array);
@@ -33,29 +36,6 @@ class MigrationContext extends SubContext
       $migrate = Migration::getInstance($machine_name);
       $migrate->processRollback();
     }
-  }
-  /**
-   * Helper function to get the destination object by source ID.
-   *
-   * @param string $migration
-   *   The machine name of the migration
-   *
-   * @param string $sourceID
-   *
-   * @return object
-   */
-  public function getLastEntity($migration, $sourceID) {
-    $mapping_table = 'migrate_map_' . strtolower($migration);
-    $eid = db_select($mapping_table, 'mt')
-      ->fields('mt', array('destid1'))
-      ->condition('sourceid1', $sourceID,'=')
-      ->execute()
-      ->fetchAssoc();
-    $eid = array_pop($eid);
-    $entity_object = entity_load('node', array($eid));
-    $entity_object = array_pop($entity_object);
-    $wrapper = entity_metadata_wrapper('node', $entity_object);
-    return $wrapper;
   }
 
   /**
@@ -92,46 +72,6 @@ class MigrationContext extends SubContext
       $expectedSuccesses,
       $successes,
       "$successes items were imported. There should have been " . $successes . ' items imported.'
-    );
-  }
-
-  /**
-   * @Given /^the "([^"]*)" migration has run for the entity with ID "([^"]*)"$/
-   */
-  public function theMigrationHasRunForTheEntityWithId($migration, $eid) {
-    $options['idlist'] = $eid;
-    $options['force'] = TRUE;
-    $this->migration = \Migration::getInstance($migration);
-    $this->migration->prepareUpdate($options);
-    $this->migration->processImport($options);
-    $completed = \Migration::RESULT_COMPLETED;
-    assertEquals(1, $completed, "The $migration migration did not complete.");
-    $this->entity = self::getLastEntity($migration, $eid);
-  }
-
-  /**
-   * @Then /^I should see "([^"]*)" for the title$/
-   */
-  public function iShouldSeeForTheTitle($title) {
-    $entity_title = $this->entity->title->value(array('sanitize' => TRUE));
-    assertNotEmpty($entity_title, "No item with the title $title was imported.");
-    assertEquals(
-      $title,
-      $entity_title,
-      "Title doesn't have the value $title. Instead it has " . $entity_title . '.'
-    );
-  }
-
-  /**
-   * @Given /^I should see "([^"]*)" for the "([^"]*)" field$/
-   */
-  public function iShouldSeeForTheField($fieldData, $field) {
-    $entity_field = $this->entity->$field;
-    var_dump($entity_field);
-    assertEquals(
-      $fieldData,
-      $entity_field,
-      "$field doesn't have the value $fieldData."
     );
   }
 
