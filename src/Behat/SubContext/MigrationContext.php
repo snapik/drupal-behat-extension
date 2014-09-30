cc3f3338e095e8ae1e62b0c71644d4349ae3d22c
Dougie Dobrzynski <doug@promethost.com>
2014-09-30 13:48:57 -0500

    Writing test code for migration features and checking field data.

--- src/Behat/SubContext/MigrationContext.php
+++ src/Behat/SubContext/MigrationContext.php
@@ -5,6 +5,10 @@ use Promet\Drupal\Behat\SubContext;
 
 class MigrationContext extends SubContext
 {
+  // Variables for use in class
+  public $entity;
+  public $migration;
+
   /**
    * Initializes context.
    */
@@ -30,105 +34,104 @@ class MigrationContext extends SubContext
       $migrate->processRollback();
     }
   }
+  /**
+   * Helper function to get the destination object by source ID.
+   *
+   * @param string $migration
+   *   The machine name of the migration
+   *
+   * @param string $sourceID
+   *
+   * @return object
+   */
+  public function getLastEntity($migration, $sourceID) {
+    $mapping_table = 'migrate_map_' . strtolower($migration);
+    $eid = db_select($mapping_table, 'mt')
+      ->fields('mt', array('destid1'))
+      ->condition('sourceid1', $sourceID,'=')
+      ->execute()
+      ->fetchAssoc();
+    $eid = array_pop($eid);
+    $entity_object = entity_load('node', array($eid));
+    $entity_object = array_pop($entity_object);
+    $wrapper = entity_metadata_wrapper('node', $entity_object);
+    return $wrapper;
+  }
 
   /**
-   * @Given /^the "([^"]*)" migration has run$/
+   * @Given /^the "([^"]*)" migration has run for entities with IDs "([^"]*)"$/
    */
-  public function theMigrationHasRun($migration, $options = array()) {
-    $migration = \Migration::getInstance($migration);
-    $migration->prepareUpdate($options);
-    $migration->processImport($options);
+  public function theMigrationHasRunForEntitiesWithIds($migration, $eids) {
+    $options['idlist'] = $eids;
+    $options['force'] = TRUE;
+    $this->migration = \Migration::getInstance($migration);
+    $this->migration->prepareUpdate($options);
+    $this->migration->processImport($options);
     $completed = \Migration::RESULT_COMPLETED;
-    var_dump($completed);
-    //$this->assertEquals(1, $completed, "The $migration migration did not complete.");
+    assertEquals(1, $completed, "The $migration migration did not complete.");
   }
 
   /**
    * @Then /^there should be (\d+) failed items$/
    */
   public function thereShouldBeFailedItems($expectedFailures) {
-    //$failures = $completed;
-    //var_dump($failures);
-    //assertEquals(
-      //$expectedFailures,
-      //$failures,
-      //"$failures items failed. There should have only been " . $expectedFailures . ' items failed.'
-    //);
+    $failures = $this->migration->processed_since_feedback - $this->migration->successes_since_feedback;
+    assertEquals(
+      $expectedFailures,
+      $failures,
+      "$failures items failed. There should have only been " . $expectedFailures . ' items failed.'
+    );
   }
 
   /**
-   * @Given /^there should be (\d+) skipped items$/
+   * @Given /^there should be (\d+) imported items$/
    */
-  public function thereShouldBeSkippedItems($skips) {
-    //throw new PendingException();
-  }
-  
-  /**
-   * @Given /^there should be (\d+) ignored items$/
-   */
-  public function thereShouldBeIgnoredItems($arg1) {
-    //throw new PendingException();
+  public function thereShouldBeImportedItems($expectedSuccesses) {
+    $successes = $this->migration->successes_since_feedback;
+    assertEquals(
+      $expectedSuccesses,
+      $successes,
+      "$successes items were imported. There should have been " . $successes . ' items imported.'
+    );
   }
 
   /**
-   * @Given /^node ID (\d+) runs on the "([^"]*)" migration$/
+   * @Given /^the "([^"]*)" migration has run for the entity with ID "([^"]*)"$/
    */
-  public function nodeIdRunsOnTheMigration($nid, $migration) {
-    $options['idlist'] = $nid;
+  public function theMigrationHasRunForTheEntityWithId($migration, $eid) {
+    $options['idlist'] = $eid;
     $options['force'] = TRUE;
-    
-    $migration = \Migration::getInstance($migration);
-    $migration->prepareUpdate($options);
-    $migration->processImport($options);
-
+    $this->migration = \Migration::getInstance($migration);
+    $this->migration->prepareUpdate($options);
+    $this->migration->processImport($options);
+    $completed = \Migration::RESULT_COMPLETED;
+    assertEquals(1, $completed, "The $migration migration did not complete.");
+    $this->entity = self::getLastEntity($migration, $eid);
   }
-
-  /**
-   * Helper function to get the NID by title.
-   *
-   * @param string $title
-   *
-   * @return string
-   */
-  protected function getLastNodeID($title) {
-    $query = new \EntityFieldQuery();
-    $entities = $query->entityCondition('entity_type', 'node')
-      ->propertyCondition('title', $title)
-      ->propertyCondition('status', 1)
-      ->execute();
-    if (!empty($entities['node'])) {
-      $node = end($entities['node']);
-    }
-    return $node->nid;
-}
 
   /**
    * @Then /^I should see "([^"]*)" for the title$/
    */
   public function iShouldSeeForTheTitle($title) {
-    
-    $nid = $this->getLastNodeID($title);
-    var_dump($nid);
-    $node = node_load($nid);
-    //var_dump($node);
-    assertNotEmpty($node, "No item with the title $title was imported.");
+    $entity_title = $this->entity->title->value(array('sanitize' => TRUE));
+    assertNotEmpty($entity_title, "No item with the title $title was imported.");
     assertEquals(
       $title,
-      $node->title,
-      "Title doesn't have the value $title. Instead it has " . $node->title . '.'
+      $entity_title,
+      "Title doesn't have the value $title. Instead it has " . $entity_title . '.'
     );
-    return $node;
   }
 
   /**
    * @Given /^I should see "([^"]*)" for the "([^"]*)" field$/
    */
   public function iShouldSeeForTheField($fieldData, $field) {
-    var_dump($node);
+    $entity_field = $this->entity->$field;
+    var_dump($entity_field);
     assertEquals(
       $fieldData,
-      $node->$field,
-      "$field doesn't have the value $fieldData. Instead it has " . $node->$field . '.'
+      $entity_field,
+      "$field doesn't have the value $fieldData."
     );
   }
 
