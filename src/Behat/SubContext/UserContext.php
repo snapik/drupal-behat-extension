7c62d95fc8fd8c474171c1a6ef04bcd4c1dc07c7
Allan Chappell <generalredneck@gmail.com>
2014-11-26 12:16:11 -0600

    Updating user exists functions.

--- src/Behat/SubContext/UserContext.php
+++ src/Behat/SubContext/UserContext.php
@@ -9,15 +9,40 @@ class UserContext extends SubContext
     return "DrupalUser";
   }
   /**
-   * @Then /^there should be a user with email "([^"]*)"$/
+   * @Then /^there (should|should not) be a user with the email "([^"]*)"$/
    */
-  public function thereShouldBeAUserWithEmail($email)
-  {
-    $efq = new \EntityFieldQuery();
-    $users = $efq->entityCondition('entity_type', 'user')
-    ->propertyCondition('mail', $email)
-    ->count()
-    ->execute();
-    $this->assertNotEquals(0, $users, 'Failed to find user with email address: ' . $email);
+  public function thereShouldBeAUserWithEmail($not, $email) {
+    $not = $not == 'should not';
+    $users = db_select('users', 'u')
+      ->fields('u', array('uid'))
+      ->condition('u.mail', $email)
+      ->execute()
+      ->fetchCol();
+    $users = count($users);
+    if ($not) {
+      assertEquals(0, $users, 'Found user with email address: ' . $email);
+    }
+    else {
+      assertNotEquals(0, $users, 'Failed to find user with email address: ' . $email);
+    }
+  }
+
+  /**
+   * @Then /^there (should|should not) be a user with the username "([^"]*)"$/
+   */
+  public function thereShouldBeAUserWithUsername($not, $username) {
+    $not = $not == 'should not';
+    $users = db_select('users', 'u')
+      ->fields('u', array('uid'))
+      ->condition('u.name', $username)
+      ->execute()
+      ->fetchCol();
+    $users = count($users);
+    if ($not) {
+      assertEquals(0, $users, 'Found user with email address: ' . $email);
+    }
+    else {
+      assertNotEquals(0, $users, 'Failed to find user with email address: ' . $email);
+    }
   }
 }
